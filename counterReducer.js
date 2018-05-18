3b59d38d6dff55fbd9279e1c89ec9b87e75d084a
김명진 [FE플랫폼] <myungjin.kim.1@navercorp.com>
2018-05-18 14:25:20 +0900

    redux with side-effect

--- counterReducer.js
+++ counterReducer.js
@@ -1,5 +1,12 @@
+const api = async () => {
+	const response = await fetch("https://api.github.com/users/JaeYeopHan/repos");
+	return response.json();
+};
+
 const PLUS = "PLUS";
 const MINUS = "MINUS";
+const FETCH_COMPLETE = "FETCH_COMPLETE";
+const FETCH_FAIL = "FETCH_FAIL";
 
 const createAction = type => payload => ({
 	type,
@@ -8,14 +15,35 @@ const createAction = type => payload => ({
 
 const plusAction = createAction(PLUS);
 const minusAction = createAction(MINUS);
+const fetchCompleteAction = createAction(FETCH_COMPLETE);
+const fetchFailAction = createAction(FETCH_FAIL);
 
 
-function counterReducer(state = 0, action) {
+const INIT_STATE = {
+	count: 0,
+	list: [],
+};
+
+function counterReducer(state = INIT_STATE, action) {
 	switch (action.type) {
 		case PLUS:
-			return state + 1;
+			return {
+				...state,
+				count: state.count + 1,
+			};
 		case MINUS:
-			return state - 1;
+			return {
+				...state,
+				count: state.count - 1,
+			};
+		case FETCH_COMPLETE:
+			const {list} = action.payload;
+			const {count} = state;
+
+			return {
+				...state,
+				list: list.slice(0, count),
+			};
 		default:
 			return state;
 	}
@@ -51,30 +79,51 @@ function createStore(reducer, state) {
 }
 
 
-const store = createStore(counterReducer, 5);
+const store = createStore(counterReducer, {count: 5, list: []});
 
 
-document.getElementById("plus").addEventListener("click", () => {
-	store.dispatch(plusAction())
+document.getElementById("plus").addEventListener("click", async () => {
+	store.dispatch(plusAction());
+
+	const repos = await api();
+
+	store.dispatch(fetchCompleteAction({
+		list: repos
+	}));
 });
-document.getElementById("minus").addEventListener("click", () => {
-	store.dispatch(minusAction())
+
+document.getElementById("minus").addEventListener("click", async () => {
+	store.dispatch(minusAction());
+
+	const repos = await api();
+
+	store.dispatch(fetchCompleteAction({
+		list: repos
+	}));
 });
 
 
 function render(state) {
-	document.getElementById("counter").innerText = state;
+	const { count, list } = state;
+	document.getElementById("counter").innerText = count;
+	document.getElementById("repos").innerHTML = list.map(item => `<li><a href="${item.html_url}">${item.full_name}</a></li>`).join("");
 }
 
 const unlisten = store.subscribe(() => {
 	render(store.getState());
 });
 
-(() => {
+(async () => {
 	render(store.getState());
+
+	const repos = await api();
+
+	store.dispatch(fetchCompleteAction({
+		list: repos
+	}));
 })();
 
-setTimeout(() => {
-	unlisten();
-	console.log('unsubscribe')
-}, 5000);
+// setTimeout(() => {
+// 	unlisten();
+// 	console.log('unsubscribe')
+// }, 5000);
